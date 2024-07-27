3e36d0d1c1eadf2299616dbd8c21a76ebcac5209
milosm97 <milos.mucha.197@gmail.com>
2024-07-27 20:21:03 +0200

    add

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
   projectId: 'dnznzv',
-  waitForFileChange: false,
+  waitForFileChange: true,
   defaultCommandTimeout: 5000,
-
+  baseUrl: "https://github.com/milosm97/MilosCypressTesting.git",
+  integrationFolder: "cypress/e2e",
+  supportFile: "cypress/support/index.js",
+  viewportWidth: 1000,
+  viewportHeight: 660,
+  video: false,
+  retries: {
+    runMode: 2,
+    openMode: 1
+           },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
