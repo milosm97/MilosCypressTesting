f0fa98eb7e52517a78b7f764764e70fc353ff190
milosm97 <milos.mucha.197@gmail.com>
2024-07-27 21:16:46 +0200

    add

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'dnznzv',
-  waitForFileChange: false,
-  defaultCommandTimeout: 5000,
-
+  projectId: '14w8m3',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
