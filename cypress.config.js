24a8261ee744bbfead780e331b5b28f2dcce8024
milosm97 <milos.mucha.197@gmail.com>
2024-07-28 11:40:33 +0200

    chrome

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '14w8m3',
+  browser: 'chrome',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
