34736cfbfed39fc6dda3d209e498a492f37fdf07
milosm97 <milos.mucha.197@gmail.com>
2024-07-28 12:03:23 +0200

    konecneJeToOK

--- cypress/e2e/4_AddAdmin.cy.js
+++ cypress/e2e/4_AddAdmin.cy.js
@@ -39,13 +39,14 @@ describe('pridanie admina', () => {
   cy.get('.oxd-autocomplete-dropdown > :nth-child(1)').click()
 
   //Username
-  cy.get(':nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-input').type("testikMMM1")
+  cy.get(':nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-input').type("testikMM1")
   //password
-  cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').type("testikMMM1")
+  cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').type("testikMM1")
   //passwordConfirm
-  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("testikMMM1")
+  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("testikMM1")
   //Save
   cy.get('.oxd-button--secondary').click()
+  cy.wait(5000)
   })
 })
 
